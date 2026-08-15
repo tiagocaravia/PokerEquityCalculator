9c231207d06b6353bc1c1ae04d2043399a3aa991
tiagocaravia-perry <scaraviaperry01@bvsd.org>
2026-08-15 00:21:47 -0600

    Fixed Redundacy

--- preflop.py
+++ preflop.py
@@ -20,17 +20,17 @@ def AggregatePreflopOdds():
                 "tie%": result['tie'],
                 "loss%": result['loss'],
             })
-    for rank1 in Card.RANKS: #Suited Sims
-            for rank2 in Card.RANKS:
-                if rank1 == rank2:
-                    continue  # Skip pairs for suited simulations
-                hole_cards = [Card(rank1, 'Diamonds'), Card(rank2, 'Diamonds')]
-                if num_simulations:
-                    num_simulations = int(num_simulations)
-                else:
-                    num_simulations = 10000
-                result = sim(hole_cards, num_players=num_players, num_simulations=num_simulations)
-                PreflopEquity.append({
+    for i in range(len(Card.RANKS)): #Suited Sims
+        rank1 = Card.RANKS[i]
+        for j in range(i + 1, len(Card.RANKS)):
+            rank2 = Card.RANKS[j]
+            hole_cards = [Card(rank1, 'Diamonds'), Card(rank2, 'Diamonds')]
+            if num_simulations:
+                num_simulations = int(num_simulations)
+            else:
+                num_simulations = 10000
+            result = sim(hole_cards, num_players=num_players, num_simulations=num_simulations)
+            PreflopEquity.append({
                     "Hand": f"{hole_cards[0]}, {hole_cards[1]}",
                     "win%": result['win'],
                     "tie%": result['tie'],
